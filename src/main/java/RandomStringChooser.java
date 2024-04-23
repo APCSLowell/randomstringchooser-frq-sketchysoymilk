69e2b1ad8395af8eab2074bfbccba64095daa18e
sketchysoymilk <143027784+sketchysoymilk@users.noreply.github.com>
2024-04-22 22:03:21 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  private ArrayList <String> array;
-  public RandomStringChooser(String [] wordArray){
-    array = new ArrayList <String>();
-    for(String word : wordArray)
-      array.add(word);
-  }
-  public String getNext(){
-    int R = (int)(Math.random*array.size());
-    if(array.size() == 0)
-      return "NONE";
-    return array.remove(r);
-  }
+    private ArrayList<String> array;
+
+    public RandomStringChooser(String[] wordArray)
+    {
+        array = new ArrayList<String>();
+
+        for(String word : wordArray)
+            array.add(word);
+    }
+
+    public String getNext()
+    {
+      if(array.size() == 0)
+            return "NONE";
+
+      int R = (int) (Math.random() * array.size());
+      return array.remove(R);
 }
